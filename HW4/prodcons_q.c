3518659964810d825e24d6c5b6854ec2ea37b449
Fidel Leal <flealca@silo.soic.indiana.edu>
2020-10-08 21:14:18 -0400

    add solution for HW4

--- HW4/prodcons_q.c
+++ HW4/prodcons_q.c
@@ -7,8 +7,8 @@
 
 // Global vars //
 char array[5];
-sem_t aphore;//, aphore2;
-sem_t mutex;
+sem_t go1, go2;
+sem_t r_mutex, w_mutex;
 
 // Function 1 //
 void* funct1 (void* arg) {
@@ -16,16 +16,15 @@ void* funct1 (void* arg) {
         char *lower = "abcdefghijklmnopqrstuvwxyz";
         int i0;
 	int* thrID0 = (int*) arg;	 
-        
-	sem_wait(&aphore);
+		sem_wait(&go1);
+		sem_wait(&w_mutex);
 	for (i0 = 0; i0 < 5; i0++) {
 		char to_buffer0 = (char) lower[rand() % 26]; 
-        	sem_wait(&mutex);
 		array[i0] = to_buffer0;
 		printf("writer ID: %d, char written: %c\n", *thrID0, to_buffer0);
-		sem_post(&mutex);
 	}
-	sem_post(&aphore);
+		sem_post(&r_mutex);
+		sem_post(&go2);
 	return 0;
 }
 
@@ -35,16 +34,15 @@ void* funct2 (void* arg) {
         char *upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int i1;
         int* thrID1 = (int*) arg;
-
-       	sem_wait(&aphore);
+		sem_wait(&go2);
+		sem_wait(&w_mutex);
         for (i1 = 0; i1 < 5; i1++) {
 		char to_buffer1 = upper[rand() % 26];
-        	sem_wait(&mutex);
 		array[i1] = to_buffer1;
         	printf("writer ID: %d, char written: %c\n", *thrID1, to_buffer1);
-		sem_post(&mutex);
 	}
-	sem_post(&aphore);
+		sem_post(&r_mutex);
+		sem_post(&go1);
 	return 0;
 }
 
@@ -53,12 +51,11 @@ void* funct2 (void* arg) {
 void* funct3 (void* arg) {
 	int i2;
         int* thrID2 = (int*) arg;
-
+		sem_wait(&r_mutex);
 	for (i2 = 0; i2 < 5; i2++) {
-		sem_wait(&mutex);
-		printf("reader ID: %d, char read: %c\n", *thrID2, array[i2]);
-		sem_post(&mutex);
-	}	
+		printf("reader ID: %d, char read: %c\n", (*thrID2), array[i2]);
+	}
+		sem_post(&w_mutex);
 	return 0;
 }
 
@@ -71,24 +68,33 @@ int main(int argc, char* argv[]) {
 
 	int i;
 	pthread_t tid[8];
-	sem_init(&mutex, 0, 1);
-	sem_init(&aphore, 0, 1);
-	//sem_init(&aphore2, 0, 0);
-
-	for (i = 0; i < 2; i ++) {
-		int toth = i;
+	sem_init(&w_mutex, 0, 1);
+	sem_init(&r_mutex, 0, 0);
+	sem_init(&go1, 0, 1);
+	sem_init(&go2, 0, 0);
+	int toth;
+	for (i = 0; i < 2; i++) {
+		toth = i;
 		pthread_create(&tid[i], NULL, funct1, (void*) &toth);
 	}
-  
-	for (i = 2; i < 4; i ++) {
-		int toth = i;	
-		pthread_create(&tid[i], NULL, funct2, (void*) &toth);
+  	int toth1;
+	for (i = 2; i < 4; i++) {
+		toth1 = i;
+		pthread_create(&tid[i], NULL, funct2, (void*) &toth1);
 	}
-
-	for (i = 0; i < 4; i ++) {
-		int toth = i;
-		pthread_create(&tid[(i + 4)], NULL, funct3, (void*) &toth);
-	}
+	int toth2=0;
+	int toth3=1;
+	int toth4=2;
+	int toth5=3;
+	pthread_create(&tid[4], NULL, funct3, (void*) &toth2);
+	pthread_create(&tid[5], NULL, funct3, (void*) &toth3);
+	pthread_create(&tid[6], NULL, funct3, (void*) &toth4);
+	pthread_create(&tid[7], NULL, funct3, (void*) &toth5);
+	//for (i = 2; i < 4; i++) {
+	//	toth2 = i ;
+//		pthread_create(&tid[i], NULL, funct3, (void*) &toth2);
+		//printf("Looooopin\");
+//	}
 
 	for (i = 0; i < 8; i ++) {
 		pthread_join(tid[i], NULL);
