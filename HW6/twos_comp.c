60467679fb563e774181cac56c1214010eed36ad
Fidel Leal <flealca@silo.soic.indiana.edu>
2020-10-19 23:42:31 -0400

    add solution for HW6

--- HW6/twos_comp.c
+++ HW6/twos_comp.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
+
+int powerof(int num, int exp) {
+	int result = 1;
+	int index = 1;
+	while(index <= exp) {
+		result = result * num;
+		index++;
+	}
+	return result;
+}
+
+//int bin_conv(char *bin) {
+//h
+	
 
 int main (int argc, char* argv[]) {
 
@@ -13,7 +26,7 @@ int main (int argc, char* argv[]) {
 	
 	int i;
 	int result = 0;
-	
+	char buff[8];	
 	for (i = 0; i < 8; i++) {
 		if ((argv[1])[i] > '1' ||  (argv[1])[i] < '0' ) {
 		printf("Error: Only binary strings accepted\n");
@@ -22,18 +35,54 @@ int main (int argc, char* argv[]) {
 	}
 	
 
-	
+	// For Unsigned //
 	if (strcmp(argv[2], "unsigned") == 0 ) {
 		for (i = 0; i < 8; i++) {
 			if ((argv[1])[i] == '1') {
-			result = (result + (pow(2,(7-i))));
+			result = (result + (powerof(2,(7-i))));
 			}
 		}
 
 	}
 	
-		
+	// For Signed //
+	else if (strcmp(argv[2], "signed") == 0 ) {
+		// If positive //
+		if ((argv[1])[0] == '0') {
+			for (i = 0; i < 8; i++) {
+				if ((argv[1])[i] == '1') {
+				result = (result + (powerof(2,(7-i))));
+				}
+			}
+		}
+		// If negative //
+		else if ((argv[1])[0] == '1') {
+			// Invert //
+			for (i = 0; i < 8; i++) {
+				if ((argv[1])[i] == '1') {
+					buff[i] = '0';
+				}
+				if ((argv[1])[i] == '0') {
+					buff[i] = '1';
+				}
+			}
+			buff[7] = '1';
+			for (i = 0; i < 8; i++) {
+					if (buff[i] == '1') {
+					result = (result + (powerof(2,(7-i))));
+					}
 
+			}
+			result = (result - (2 * result));		
+		}
+		
+			
+	}
+
+	else {
+		printf("Error: must specify signed or unsigned\n");
+		return 1;
+	}	
 	
 	
 printf("%d\n", result);
